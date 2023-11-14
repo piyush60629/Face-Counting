431794121a9c2fb055c5c3ddbb05b9f5119196a0
piyush60629 <68905928+piyush60629@users.noreply.github.com>
2023-11-14 09:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,53 @@
 from flask import Flask, render_template, Response
 import cv2
 import json
-import os  # Import the os module
-import atexit
 
 app = Flask(__name__)
 
-# Get camera index from environment variable, default to 0
-camera_index = int(os.environ.get('CAMERA_INDEX', 0))
-cap = cv2.VideoCapture(camera_index)
+def detect_faces(frame):
+    # Use Haarcascade for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
 
-# Use relative path for the cascade classifier XML file
-cascade_path = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascade_path)
+    count = len(faces)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return count
 
 def generate_frames():
+    camera_index = 0  # You can change this index based on your requirements
+
+    cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
+
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
+
     while True:
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if not ret:
+            break
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
+        # Detect faces and get the count
+        face_count = detect_faces(frame)
 
-        count = len(faces)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+        # Encode the frame for streaming
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        yield f"data: {json.dumps({'count': count})}\n\n"
-
-# Explicitly release video capture resources on application exit
-def release_resources():
-    cap.release()
-
-# Register the release_resources function to be called on application exit
-atexit.register(release_resources)
+        # Send face count as JSON data
+        yield f"data: {json.dumps({'count': face_count})}\n\n"
 
 @app.route('/')
 def index():
@@ -55,9 +57,8 @@ def index():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/update_count')
-def update_count():
-    return Response(generate_frames(), content_type='text/event-stream')
-
 if __name__ == "__main__":
-    app.run(port=8000,debug=True) # running the app on the local machine on port 8000
+    # Load the Haarcascade classifier
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    app.run(port=8000, debug=True)
